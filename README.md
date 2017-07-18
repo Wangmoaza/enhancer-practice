547c776e5cbc199bc476e1bf069807402cbdbe6f
Ha-Eun Hwangbo <haeun0512@hanmail.net>
2017-07-18 19:56:25 +0900

    first draft

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Lab note for enhancer practice session
 
+### 2017-07-17
+* parsing data using pandas pandas library
+* doing tutorials
+
+### 2017-07-18
+* dhs, chip-seq, refGene data conversion to pandas data frame - DONE.
+* finished first draft.
+* error raised during execution. need to debug tomorrow.
+
+
--- enhancer_predicton.py
+++ enhancer_predicton.py
@@ -2,78 +2,138 @@
 # This project is to practice predicting enhancer sequences
 # using refGene.txt, ChIA-PET data, and DNase-seq data
 # Author: Ha-Eun Hwangbo
-# Date: 2017/07/10
+# Date: 2017/07/18
 
-HumanChrList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y']
+import pandas as pd
+import numpy as np
+import re
 
-class Promoter:
-	def __init__ (self, tokens):
-		""" parses refGene.txt file to obtain necessary fields.
-		caller should check for coding/noncoding and haplotype.
+def get_data(dhs_file, chip_file, refGene_file):
+	""" Convert data files to desired data frames.
+		args:
+			dhs_file (string): path to DHS narrowPeak file
+			chip_file (string): path to chIP-seq bed file
+			refGene_file (string): path to refGene.txt
 
-		Args:
-			line (string): refGene.txt line
-
-		refGene.txt header fields
-		0 `bin` smallint(5) unsigned NOT NULL,
-		1 `name` varchar(255) NOT NULL,
-		2 `chrom` varchar(255) NOT NULL,
-		3 `strand` char(1) NOT NULL,
-		4 `txStart` int(10) unsigned NOT NULL,
-		5 `txEnd` int(10) unsigned NOT NULL,
-		...
-		""" 
-		self.name = tokens[1]
-		self.chr = int(tokens[2][3:])
-		self.strand = tokens[3]
-
-		tss = 0
-		if self.strand == "+":
-			tss = int(tokens[4])
-		elif self.strand == "-":
-			tss = int(tokens[5])
-		else
-			print("Error: invalid strand info")
-			return
-
-		self.start = tss - 1000
-
-	def parse_refGene(self, tokes):
-
-		self.end = tss + 1000
-	### END - refGene
-
-	def name(self):
-		return self.name
-
-	def start(self):
-		return self.start
-
-	def end(self):
-		return self.end
+		returns:
+			tuple of dhs, chip, promoter data frames
+	"""
 
 
-class DHS:
-	def __init__(self, tokens):
-		""" parses DHS peak bed file.
-		file format:
-		chr#	start 	end 	peakname	score
-		chr1	10084	10085	NA_peak_1	22.52676
-		"""
+	# convert dhs narrowPeak file to pandas dataframe
+	# format: chrom / start / end
+	dhs = pd.read_table(dhs_file, sep='\t', header=None, index_col=False,
+                         names=['chrom', 'start', 'end','name', 'score', 'strand', 'signalValue', 'pValue', 'qValue', 'peak'])
+	dhs.drop(['signalValue', 'pValue', 'qValue', 'strand', 'name', 'score', 'peak'], axis=1, inplace=True)
+	dhs = dhs.apply(pd.to_numeric, errors='ignore')
 
-		self.chr = tokens[0][3:]
-		self.start = int(tokens[1])
-		self.end = int(tokens[2])
+	# convert chIP-seq bed file to pandas dataframe
+	# format: chrom1 / start1 / end1 / chrom2 / start2 / end2 / score
+	chip = pd.read_table(chip_file, sep='\t', header=None, index_col=False,
+                     names=['chrom', 'start', 'end', 'interaction', 'score', 'strand', 'unknown', 'unknown2', 'itemRGB', 'unknown3', 'length'])
+	chip.drop(['score', 'strand', 'unknown', 'unknown2', 'itemRGB', 'unknown3', 'length'], axis=1, inplace=True)
+	groups = "(?P<chrom1>chr\w+)[:](?P<start1>\d+)(..)(?P<end1>\d+)[-](?P<chrom2>chr\w+)[:](?P<start2>\d+)(..)(?P<end2>\d+)[,](?P<score>\d+)"
+	chip = chip['interaction'].str.extract(groups, expand=True)
+	chip.drop([2, 6], axis=1, inplace=True)  # remove (..) columns
+	chip.drop_duplicates(inplace=True)
+	chip = chip.apply(pd.to_numeric, errors='ignore')
 
+	# convert refGene.txt file to pandas dataframe
+	# format: name / chrom / strand / promStart / promEnd
+	refGene = pd.read_table(refGene_file, sep='\t', header=None, index_col=False,
+                         names=['bin', 'name', 'chrom', 'strand', 'txStart', 'txEnd'] + range(10))
+	refGene.drop(['bin'] + range(10), axis=1, inplace=True) # drop unnecessary columns
+	refGene.drop(refGene[refGene.name.str.startswith('NR_')].index, axis=0, inplace=True)  # drop noncoding genes
+	# drop haplotype genes
+	pattern = 'chr\w+[_]\w+'
+	refGene.drop(refGene[refGene.chrom.str.contains(pattern)].index, axis=0, inplace=True)
 
-class ChIA-PET
+	# make promoter data frame
+	prom = []
+	for idx, row in refGene.iterrows():
+		if row.strand == '-':
+			prom.append(row.txEnd)
+		elif row.strand == '+':
+			prom.append(row.txStart)
+	### END - for
+	prom = np.asarray(prom)
+	promoter = pd.DataFrame({'promStart': prom - 1000, 'promEnd': prom + 1000}, index=refGene.index)
+	promoter = pd.concat([refGene, promoter], axis=1)
+	promoter.drop(['txStart', 'txEnd'], axis=1, inplace=True)
 
+	# sort by gene names and drop genes that have same promoters (keep the least numbered gene)
+	# FIXME should I delete genes that have overlapping promoters?
+	promoter.sort_values('name', axis=0, inplace=True)
+	promoter.drop_duplicates(subset=['promEnd', 'promStart'], inplace=True)
 
-def check_validity(tokens):
-	if tokens[1][:2] != "NM":
-		return False
+	promoter = promoter[['name', 'chrom', 'strand', 'promStart', 'promEnd']] # reorder columns
+	print "*** importing file DONE ***"
+	return dhs, chip, promoter
+### END - get_data
 
-	if tokens[2][3:] not in HumanChrList:
-		return False
+def match(dhs, chip, promoter):
+	""" match promoter-chip-dhs to make data frame of promoter-enhancer interaction
+	args:
+		dhs (pandas DataFrame): dhs data frame
+		chip (pandas DataFrame): chip-seq data frame
+		promoter (pandas DataFrame): prmoter information data frame
 
-	return True
+	returns:
+		resulting promoter-interaction DataFrame
+	"""
+	out_arr = np.empty((0, 8), dtype=object) # value ndarray for matchedProm data frame
+	for chipIdx, chipRow in chip.iterrows():
+		# match promoter to chip
+		# consider as matched if overlapped even by 1 base
+		condition1 = (promoter.chrom == chipRow.chrom1) & (promoter.promEnd > chipRow.start1) & (promoter.promStart < chipRow.end1)
+		condition2 = (promoter.chrom == chipRow.chrom2) & (promoter.promEnd > chipRow.start2) & (promoter.promStart < chipRow.end2)
+		tmp_df1 = promoter[condition1]
+		tmp_df2 = promoter[condition2]
+
+		# np ndarray of values, fill empty tmp_out array with elements
+		m1, m2 = tmp_df1.shape[0], tmp_df2.shape[0] 
+		r = promoter.shape[1]
+		tmp_out = np.empty((m1+m2,r+3),dtype=object)
+		# matched with first seq block
+		tmp_out[:m1, :r] = tmp_df1.values[:, :]
+		tmp_out[:m1, r:r+3] = np.array([chipRow.start2, chipRow.end2])
+		# matched with second seq block
+		tmp_out[m1:m1+m2, :r] = tmp_df2.values[:, :]
+		tmp_out[m1:m1+m2, r:r+3] = np.array([chipRow.start1, chipRow.end1])
+		# stack output array
+		out_arr = np.vstack((out_arr, tmp_out))
+	### END - for
+	print "*** matching promoter to chip DONE ***"
+	matchedProm = pd.DataFrame(out_arr, columns=['name', 'chrom', 'strand', 'promStart', 'promEnd', 'enChrom', 'enStart', 'enEnd'])
+
+	# remove promoter-promoter interaction
+	for promIdx, promRow in promoter.iterrows():
+		isPromoter = (promRow.chrom == matchedProm.enChrom) & (promRow.promEnd > matchedProm.enStart) & (promRow.promStart < matchedProm.enEnd)
+		matchedProm = matchedProm.drop(matchedProm[isPromoter].index)
+	### END - for
+	print "*** removing promoter-promoter interaction DONE ***"
+
+	# match dhs to matchedProm
+	result_arr = np.empty((0, 8), dtype=object)
+	for dhsIdx, dhsRow in dhs.iterrows():
+		condition = (dhsRow.chrom == matchedProm.enChrom) & (dhsRow.end > matchedProm.enStart) & (dhsRow.start < matchedProm.enEnd)
+		tmp_df = matchedProm[condition]
+		m, r = tmp_df.shape[0], matchedProm.shape[1]
+		tmp_out = np.empty((m, r), dtype=object)
+		# change chip-seq value to dhs start end value
+		tmp_out[:, :-2] = tmp_df.values[:, :-2]
+		tmp_out[:, -2:] = dhsRow.values[-2:]
+		result_arr = np.vstack((result_arr, tmp_out))
+	### END - for
+	print "*** matching dhs to matchedProm DONE"
+	result = pd.DataFrame(result_arr, columns=['name', 'chrom', 'strand', 'promStart', 'promEnd', 'enChrom', 'enStart', 'enEnd'])
+	return result
+### END - match
+
+def main():
+	dhs, chip, promoter = get_data("../NA_peaks.narrowPeak", "../wgEncodeGisChiaPetK562Pol2InteractionsRep1.bed", "../refGene.txt")
+	result = match(dhs, chip, promoter)
+	result.to_csv('promoter-enhancer.csv')
+
+if __name__ == "__main__":
+	main()
